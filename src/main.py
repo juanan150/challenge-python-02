701b6018180085fc148c4ef6cee9414b19ba3081
Juan Andres Quiroz <juanan0309@gmail.com>
2020-05-08 08:47:21 -0500

    Challenge Solved
    :q

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
 # Resolve the problem!!
 import string
+import random
 
 SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
 
 
 def generate_password():
-    # Start coding here
-
+    pass_len = (random.randint(8, 16))
+    password = [' '] * pass_len
+    while ' ' in password:
+        empty_spaces = [x for x in range(len(password)) if password[x] == ' ']
+        if len(empty_spaces) > 0:
+            password[random.choice(empty_spaces)] = string.ascii_lowercase[random.randint(0,len(string.ascii_lowercase))-1]
+        
+        if len(empty_spaces) > 1:
+            password[random.choice(empty_spaces)] = string.ascii_uppercase[random.randint(0,len(string.ascii_uppercase))-1]
+        
+        if len(empty_spaces) > 2:
+            password[random.choice(empty_spaces)] = string.digits[random.randint(0,len(string.digits))-1]
+        
+        if len(empty_spaces) > 3:
+            password[random.choice(empty_spaces)] = random.choice(SYMBOLS)
+    
+    password_str = ''.join(x for x in password)
+    return password_str
 
 def validate(password):
 
